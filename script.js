d83bd38314f552cbe74ea5e6b95288af14ceef9f
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-03-30 17:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const fname=document.querySelector('input[name='fname']').value
-	const lname = document.querySelector('input[name='lname']').value
-	alert(fname+' '+lname')
+	const fname = document.querySelector('input[name="fname"]').value;
+    const lname = document.querySelector('input[name="lname"]').value;alert(fname+' '+lname')
 	
 }
